5964ebb6153caea86a1272575bf6e7d2a1659a94
matthias-franke <matthias.franke@conetex.com>
2026-05-28 19:51:00 +0000

    Deploying to gh-pages from @ conetex/org-conetex-runtime-instrument@b4c1b50d76e625623e2605ad9fe8faa0c2d85388 🚀

---
+++ latest-snapshot/apiDoc/example/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.conetex.runtime.instrument.example.subpackage","l":"ClassFromOtherPackage"},{"p":"org.conetex.runtime.instrument.example","l":"Main"},{"p":"org.conetex.runtime.instrument.example","l":"Methods"}];updateSearchResults();
