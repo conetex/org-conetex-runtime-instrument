b631e5b4fd704edd255b8042950c08c34350a936
matthias franke <matthias.franke@conetex.com>
2026-05-29 21:12:58 +0200

    changed version to release

--- versionSnapshotToRelease.bat
+++ versionSnapshotToRelease.bat
@@ -8,7 +8,7 @@ for /f "delims=" %%v in ('mvn -q help:evaluate "-Dexpression=project.version" -D
 )
 
 if "%CURRENT_VERSION%"=="" (
-    echo Konnte aktuelle Version nicht ermitteln.
+    echo can not extract project.version from pom
     exit /b 1
 )
 
@@ -18,29 +18,15 @@ REM 2) handle -SNAPSHOT
 REM lenght of "-SNAPSHOT" is 9
 set LAST9=%CURRENT_VERSION:~-9%
 if "%LAST9%"=="-SNAPSHOT" (
-    set BASE_VERSION=%CURRENT_VERSION:~0,-9%
+    set NEW_VERSION=%CURRENT_VERSION:~0,-9%
 ) else (
-    set BASE_VERSION=%CURRENT_VERSION%
+    echo ERROR: current version is already release
+    exit /b 1
 )
 
-REM 3) split major.minor.patch
-for /f "tokens=1-3 delims=." %%a in ("%BASE_VERSION%") do (
-    set MAJOR=%%a
-    set MINOR=%%b
-    set PATCH=%%c
-)
+echo new version: %NEW_VERSION%
 
-if "%PATCH%"=="" set PATCH=0
-
-REM 4) increase Patch
-set /a PATCH=PATCH+1
-
-REM 5) build new version string
-set NEW_VERSION=%MAJOR%.%MINOR%.%PATCH%-SNAPSHOT
-
-echo Neue Version: %NEW_VERSION%
-
-REM 6) set new version string
+REM 3) set new version string
 mvn versions:set -DnewVersion=%NEW_VERSION%
 
 endlocal
