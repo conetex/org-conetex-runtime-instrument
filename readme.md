ef61ff91b52b3e8923f500e20667295b40f9affb
matthias-franke <matthias.franke@conetex.com>
2026-05-29 21:14:50 +0200

    feature/reworkStuff (#10)
    * updated site link in readme.md
    
    * do build only on main and *site*
    
    * changed master to main
    
    * changed version to release
    
    ---------
    
    Co-authored-by: mfr

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ## docs:
+- [latest-SNAPSHOT](https://conetex.github.io/org-conetex-runtime-instrument/latest-SNAPSHOT/index.html)
 - [latest](https://conetex.github.io/org-conetex-runtime-instrument/latest/index.html)
 - [0.0.1](https://conetex.github.io/org-conetex-runtime-instrument/0.0.1/index.html)
---
+++ versionIncrementSnapshot.bat
@@ -0,0 +1,47 @@
+REM this script is just magic around this simple command: mvn versions:set -DnewVersion=%NEW_VERSION%
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+REM 1) get version from maven
+for /f "delims=" %%v in ('mvn -q help:evaluate "-Dexpression=project.version" -DforceStdout') do (
+    set CURRENT_VERSION=%%v
+)
+
+if "%CURRENT_VERSION%"=="" (
+    echo can not extract project.version from pom
+    exit /b 1
+)
+
+echo Current Version: %CURRENT_VERSION%
+
+REM 2) handle -SNAPSHOT
+REM lenght of "-SNAPSHOT" is 9
+set LAST9=%CURRENT_VERSION:~-9%
+if "%LAST9%"=="-SNAPSHOT" (
+    set BASE_VERSION=%CURRENT_VERSION:~0,-9%
+) else (
+    set BASE_VERSION=%CURRENT_VERSION%
+    echo current version is release, new version will be snapshot
+)
+
+REM 3) split major.minor.patch
+for /f "tokens=1-3 delims=." %%a in ("%BASE_VERSION%") do (
+    set MAJOR=%%a
+    set MINOR=%%b
+    set PATCH=%%c
+)
+
+if "%PATCH%"=="" set PATCH=0
+
+REM 4) increase Patch
+set /a PATCH=PATCH+1
+
+REM 5) build new version string
+set NEW_VERSION=%MAJOR%.%MINOR%.%PATCH%-SNAPSHOT
+
+echo new version: %NEW_VERSION%
+
+REM 6) set new version string
+mvn versions:set -DnewVersion=%NEW_VERSION%
+
+endlocal
